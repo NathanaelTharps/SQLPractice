32cece99ea554817892d4b06bc08b8f019a3be6f
NathanaelTharps <32499444+NathanaelTharps@users.noreply.github.com>
2018-04-24 17:47:42 -0500

    Create mytable.sql

---
+++ mytable.sql
@@ -0,0 +1,8 @@
+  CREATE TABLE BOOKS_BIBLE(id INT, name TEXT, verse INT);
+   
+   INSERT INTO BOOKS_BIBLE (id, name, verse)
+   
+   VALUES 
+   (1, Gensis, 1)
+   (2, Exodus, 22)
+   (6, Deuteronomy, 32)
